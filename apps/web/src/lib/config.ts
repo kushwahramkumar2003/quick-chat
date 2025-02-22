49a3f2630d4db4015664845ffd66a457c7ee5720
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2025-02-22 23:06:32 +0530

    baseurl changed

--- apps/web/src/lib/config.ts
+++ apps/web/src/lib/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-  wsUrl: import.meta.env.VITE_WS_URL || "ws://localhost:8081",
-  httpUrl: import.meta.env.VITE_HTTP_URL || "http://localhost:8080",
+  wsUrl: import.meta.env.VITE_WS_URL,
+  httpUrl: import.meta.env.VITE_HTTP_URL,
   admin: import.meta.env.VITE_ADMIN || "",
   isAdminRole: import.meta.env.VITE_IS_ADMIN_ROLE || "",
 };
