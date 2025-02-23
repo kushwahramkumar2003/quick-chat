6f08e87039cacc83d75e63ce8c4f261b394cbf80
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2025-02-23 16:50:23 +0530

    auth bug fixed

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -1,18 +1,19 @@
 import axios from "axios";
 import { config } from "./config";
+import { getWithExpiry } from "./utils";
 
 const api = axios.create({
   baseURL: config.httpUrl,
   withCredentials: true,
 });
 
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
+api.interceptors.request.use((config) => {
+  const token = getWithExpiry("token");
+  if (token) {
+    config.headers.Authorization = `token ${token}`;
+  }
+  return config;
+});
 
 export const auth = {
   login: async (email: string, password: string) => {
